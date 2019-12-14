35162e7b1e232b6a622daa5ac3c19141e35918b6
dingchaofan <627844006@qq.com>
2019-12-15 05:05:01 +0800

    换轮播json读取方式

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -5,8 +5,6 @@ import echarts from 'echarts';
 import 'echarts-gl';
 import mapboxgl from 'mapbox-gl';
 // import $ from  'jquery';
-import * as fs from 'fs';  
-import * as path from 'path'; // 导入fs库和path库
 
 /**
  * @description
@@ -48,13 +46,23 @@ class Analysis extends Component{
                 // （1）首先找到原本样本数据X的最小值Min及最大值Max
                 // （2）计算系数：k=（b-a)/(Max-Min)
                 // （3）得到归一化到[a,b]区间的数据：Y=a+k(X-Min) 或者 Y=b+k(X-Max)
-                var k = 4900/(0.9-0.1)
-                data[i][2] = 100 + k*(data[i][2]-0.1)
+                var k = 499/(0.9-0.1)
+                data[i][2] = 1 + k*(data[i][2]-0.1)
             }
-            console.log('load json path:'+jsonPath);
+            // console.log('load json path:'+jsonPath);
             var datatime = jsonPath.split('/')[jsonPath.split('/').length - 1].split('.')[0]
-            console.log('load json data time:'+datatime);
-            this.showmapbox(data,datatime);
+            // console.log('load json data time:'+datatime);
+            // this.showmapbox(data,datatime);
+
+            this.myChartGl.setOption({
+                title: { 
+                    subtext: datatime, //"2019-12-13 14:00", //主标题的副标题文本内容，如果需要副标题就配置这一项
+                },
+                series: [{
+                    data: data,
+                }]
+
+            });
         })
     };
 
@@ -63,32 +71,73 @@ class Analysis extends Component{
         // 根据文件路径读取文件，返回一个文件列表
         // var dirPath = jsonNameFilePath
         jsonNameFilePath = "./data/2019_04_02_all_data-json/jsonNameList.json"
+
+        var data = require('../data/jsonNameList.json').namelist;
+ 
+        var len = data.length
+        // for(var i = 0, len = data.length; i < len; i++){
+        //     var jsonPath = jsonNameFilePath.split('jsonName')[0]+data[i]
+        //     // setTimeout(function(){console.log('123')},5000);
+        //     setTimeout(() => {
+        //         console.log('123')
+        //         this.loaddata(jsonPath)
+        //     }, 2000);
+        // }
+        this.lunbo = -1*this.lunbo
+        if(this.lunbo === -1){
+            console.log("lunbo stop")
+            clearInterval(this.settimer)
+        }
+        else{
+            this.lunbo = 1
+            console.log("lunbo start")
+            this.settimer = setInterval(() => {
+                if(this.lunbo_i<len){
+                    var jsonPath = jsonNameFilePath.split('jsonName')[0]+data[this.lunbo_i]
+                    console.log(jsonPath)
+                    this.loaddata(jsonPath)
+                    this.lunbo_i += 1 
+                }
+                else{
+                    this.lunbo_i = 0
+                }
+            }, 1000);
+        };
         
-        fetch(jsonNameFilePath)
-        .then(res => res.json())
-        .then(json => {
-            var data = json.namelist
-            console.log(data)
-            for(var i = 0, len = data.length; i < len; i++){
-                var jsonPath = jsonNameFilePath.split('jsonName')[0]+data[i]
-                console.log(jsonPath)
-                // this.loaddata(jsonPath)
-                // setInterval(function (jsonPath) {
-                //     this.loaddata(jsonPath);
-                // },2000);
-            }
-            // console.log('load json path:'+jsonPath);
-            // var datatime = jsonPath.split('/')[jsonPath.split('/').length - 1].split('.')[0]
-            // console.log('load json data time:'+datatime);
-            // this.showmapbox(data,datatime);
-        })
+        // this.lunbo = -1*this.lunbo
+        // if(this.lunbo === -1){
+        //     console.log("lunbo stop")
+        // }
+        // else{
+        //     this.lunbo = 1
+        //     console.log("lunbo start")
+
+        //     fetch(jsonNameFilePath)
+        //     .then(res => res.json())
+        //     .then(json => {
+        //         var data = json.namelist
+        //         // console.log(data)
+        //         this.datalist = data
+        //         for(var i = 0, len = data.length; i < len; i++){
+        //             var jsonPath = jsonNameFilePath.split('jsonName')[0]+data[i]
+        //             // console.log(jsonPath)
+        //             // this.loaddata(jsonPath)
+        //             // setTimeout(function(){console.log('123')},5000);
+        //             // setInterval(() => {
+        //             //     console.log('123')
+        //             // }, 1200);
+        //         }
+        //     })
+        // };
+        
+        
         
     };
 
     showmapbox = (data=[],datatime="") => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiaHVzdDEyIiwiYSI6ImNrM3BpbDhsYTAzbDgzY3J2OXBzdXFuNDMifQ.bDD9-o_SB4fR0UXzYLy9gg';
         
-        var myChart = echarts.init(document.getElementById('mapbox_echartgl'));
+        this.myChartGl = echarts.init(document.getElementById('mapbox_echartgl'));
 
         // 模拟数据
         var data0 = [
@@ -108,7 +157,7 @@ class Analysis extends Component{
             [116.342587,40.059276,8000],
             ]
         
-        myChart.setOption({
+        this.myChartGl.setOption({
             title: { 
                 text: '交通三维柱状图',
                 padding:20,//各个方向的内边距，默认是5，可以自行设置
@@ -122,7 +171,7 @@ class Analysis extends Component{
 				},
             },
             visualMap: {
-                type: 'continuous',
+                // type: 'continuous',
                 show: true, //是否显示 visualMap-continuous 组件。如果设置为 false，不会显示，但是数据映射的功能还存在。
                 calculable: true, //是否显示拖拽用的手柄（手柄能拖拽调整选中范围）
                 realtime: true, //拖拽时，是否实时更新。
@@ -131,22 +180,32 @@ class Analysis extends Component{
                 bottom:40,
                 dimension: 2, //指定用数据的『哪个维度』，映射到视觉元素上,默认取 data 中最后一个维度。
                 
-                inRange: {
-                    // color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026'],
-                    color:['#2c7bb6','#abd9e9','#ffffbf','#fdae61','#d7191c'],
-                    symbolSize: [10,10],
-                },
-                outOfRange: {
-                    colorAlpha: 0
-                },
-                min: 100,
-                max: 5000,
+                // inRange: {
+                //     // color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026'],
+                //     // color:['#abd9e9','green','orign','#d7191c'],
+                //     // color:['#64e291','#e6e56c','#eb7070','#d7191c'],
+                //     // color:['#eb7070','#d7191c'],
+                //     symbolSize: [10,10],
+                // },
+                // outOfRange: {
+                //     colorAlpha: 0
+                // },
+                color: ['red','#eac736','green'],
+                pieces: [
+                    {min: 350}, // 不指定 max，表示 max 为无限大（Infinity）。
+                    {min: 200, max: 350},
+                    {min: 100, max: 200},
+                    {min: 0, max: 100, label: '0 到 1000（自定义label）'},
+                ],
+                min: 1,
+                max: 500,
             },
             mapbox3D: {
                 // Mapbox 地图中心经纬度,经纬度用数组表示
-                center: [116.368608,39.901744],
+                // center: [116.368608,39.901744],
+                center: [116.388608,39.881744],
                 // Mapbox 地图的缩放等级
-                zoom: 10,
+                zoom: 11,
                 // Mapbox 地图样式
                 style: 'mapbox://styles/mapbox/streets-v8',
                 // 视角俯视的倾斜角度,默认为0，也就是正对着地图。最大60。
@@ -184,10 +243,10 @@ class Analysis extends Component{
                 type: 'bar3D',
                 coordinateSystem: 'mapbox3D',
                 shading: 'color',
-                bevelSize: 0.3,
-                minHeight: 100,
-                maxHeight: 5000,
-                barSize: 0.05,
+                bevelSize: 1,
+                minHeight: 1,
+                maxHeight: 500,
+                barSize: 0.1,
                 data: data,
                 // 图形是否不响应和触发鼠标事件，默认为 false，即响应和触发鼠标事件。
                 silent: true,
@@ -196,20 +255,27 @@ class Analysis extends Component{
             }]
         });
         // 获取mapbox对象
-        var mapbox = myChart.getModel().getComponent('mapbox3D').getMapbox();
+        var mapbox = this.myChartGl.getModel().getComponent('mapbox3D').getMapbox();
 
-        // 判断NavigationControl控件是否存在
-        if(this.nav){
-            console.log("NavigationControl exist")
-        }
-        else{
-            this.nav = new mapboxgl.NavigationControl({
-                zoomIn: true,
-                zoomOut: true,
-                compass: true,
-            });
-            mapbox.addControl(this.nav, 'top-right');
-        }
+        this.nav = new mapboxgl.NavigationControl({
+            zoomIn: true,
+            zoomOut: true,
+            compass: true,
+        });
+        mapbox.addControl(this.nav, 'top-right');
+
+        // // 判断NavigationControl控件是否存在
+        // if(this.nav){
+        //     console.log("NavigationControl exist")
+        // }
+        // else{
+        //     this.nav = new mapboxgl.NavigationControl({
+        //         zoomIn: true,
+        //         zoomOut: true,
+        //         compass: true,
+        //     });
+        //     mapbox.addControl(this.nav, 'top-right');
+        // }
         // window.addEventListener("resize",function(){
         //     myChart.resize();
         // });
@@ -360,9 +426,18 @@ class Analysis extends Component{
                             title="分析页"
                             subTitle="交通数据分析"
                             extra={[
-                                <Button key="1" type="primary" onClick={() => {this.loaddata("./data/test.json")}}>测试数据</Button>,
+                                <Button key="1" type="primary" onClick={() => {this.loaddata("./data/2019-04-02_09-00.json")}}>测试数据</Button>,
                                 <Button key="2" type="primary" onClick={() => {this.load_multi_data()}}>数据轮播</Button>,
-                                <Button key="3" onClick={() => {this.showmapbox()}}>地图重置</Button>,
+                                <Button key="3" 
+                                    onClick={() => {
+                                        this.myChartGl.clear(); 
+                                        this.showmapbox(); 
+                                        clearInterval(this.settimer); 
+                                        this.lunbo = -1; 
+                                        this.lunbo_i=0
+                                    }
+                                    }>地图重置
+                                </Button>,
                             ]}
                         />
                     </Col>
@@ -372,10 +447,10 @@ class Analysis extends Component{
                         <div id="mapbox_echartgl" style={{minWidth: 400, minHeight: 600}}></div>
                     </Col>
                 </Row>
-                <Row gutter={[16, 16]}>
+                {/* <Row gutter={[16, 16]}>
                     <Col span={12}><div id="point_analysis" style={{Width: 200, height: 250}}></div></Col>
                     <Col span={12}><div id="full_analysis" style={{Width: 200, minHeight: 300}}></div></Col>
-                </Row>
+                </Row> */}
             </div>
         );
     };
