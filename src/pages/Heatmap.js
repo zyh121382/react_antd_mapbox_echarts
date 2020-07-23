fd0738501d0c1e70f5b127e38fe0f0f4b4adbfa7
dingchaofan <dingchaofan@kingsoft.com>
2020-07-23 15:17:37 +0800

    docs:重新运行项目，修改gitignore配置

--- src/pages/Heatmap.js
+++ src/pages/Heatmap.js
@@ -64,9 +64,11 @@ class Heatmap extends Component {
         map.on('load', function () {
             // Add a geojson point source.
             // Heatmap layers also work with a vector tile source.
+            
             map.addSource('earthquakes', {
                 'type': 'geojson',
                 'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
+                // 'data': '../src/data/earthquakes.geojson'
             });
 
             map.addLayer(
@@ -143,57 +145,57 @@ class Heatmap extends Component {
                 'waterway-label'
             );
 
-            // map.addLayer(
-            //     {
-            //         'id': 'earthquakes-point',
-            //         'type': 'circle',
-            //         'source': 'earthquakes',
-            //         'minzoom': 7,
-            //         'paint': {
-            //             // Size circle radius by earthquake magnitude and zoom level
-            //             'circle-radius': [
-            //                 'interpolate',
-            //                 ['linear'],
-            //                 ['zoom'],
-            //                 7,
-            //                 ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
-            //                 16,
-            //                 ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
-            //             ],
-            //             // Color circle by earthquake magnitude
-            //             'circle-color': [
-            //                 'interpolate',
-            //                 ['linear'],
-            //                 ['get', 'mag'],
-            //                 1,
-            //                 'rgba(33,102,172,0)',
-            //                 2,
-            //                 'rgb(103,169,207)',
-            //                 3,
-            //                 'rgb(209,229,240)',
-            //                 4,
-            //                 'rgb(253,219,199)',
-            //                 5,
-            //                 'rgb(239,138,98)',
-            //                 6,
-            //                 'rgb(178,24,43)'
-            //             ],
-            //             'circle-stroke-color': 'white',
-            //             'circle-stroke-width': 1,
-            //             // Transition from heatmap to circle layer by zoom level
-            //             'circle-opacity': [
-            //                 'interpolate',
-            //                 ['linear'],
-            //                 ['zoom'],
-            //                 7,
-            //                 0,
-            //                 8,
-            //                 1
-            //             ]
-            //         }
-            //     },
-            //     'waterway-label'
-            // );
+            map.addLayer(
+                {
+                    'id': 'earthquakes-point',
+                    'type': 'circle',
+                    'source': 'earthquakes',
+                    'minzoom': 7,
+                    'paint': {
+                        // Size circle radius by earthquake magnitude and zoom level
+                        'circle-radius': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            7,
+                            ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                            16,
+                            ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+                        ],
+                        // Color circle by earthquake magnitude
+                        'circle-color': [
+                            'interpolate',
+                            ['linear'],
+                            ['get', 'mag'],
+                            1,
+                            'rgba(33,102,172,0)',
+                            2,
+                            'rgb(103,169,207)',
+                            3,
+                            'rgb(209,229,240)',
+                            4,
+                            'rgb(253,219,199)',
+                            5,
+                            'rgb(239,138,98)',
+                            6,
+                            'rgb(178,24,43)'
+                        ],
+                        'circle-stroke-color': 'white',
+                        'circle-stroke-width': 1,
+                        // Transition from heatmap to circle layer by zoom level
+                        'circle-opacity': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            7,
+                            0,
+                            8,
+                            1
+                        ]
+                    }
+                },
+                'waterway-label'
+            );
         });
 
         map.on('zoomend', function() {
